d4d25c5d5a03b7c316be09eca343605d71c5fecb
jhassan <jawadjee0519@gmail.com>
2019-10-17 03:07:13 +0500

    updates with eloquent

--- app/Account.php
+++ app/Account.php
@@ -8,8 +8,39 @@ use DB;
 class Account extends Model
 {
   // View All Vouchers
+
+  public $table = 'vouchermaster';
+
+
+  public function shop(){
+    return $this->belongsTo('App\Shops', 'shop_id', 'id');
+  }
+
+
   public function all_vouchers($shop_id, $start_date, $end_date)
   {
+    if(empty($shop_id)):
+      $abc = Account::with('shop')->orderBy('vm_date', 'desc')->get();
+    else:
+      $abc = Account::with('shop')->orderBy('vm_date', 'desc')
+        ->where(['shop_id' => $shop_id, 'vm_date >=' => $start_date, 'vm_date <=', $end_date])->get();
+    endif;  
+    
+
+// array(
+//   '' => '',
+//   '' => '',
+//   '' => '',
+//   'shops' => array(
+//       [0] => array(
+//           '' => ''
+//       )
+//     )
+// )
+
+echo '<pre>';
+print_r($abc->toArray());
+die;
     if(empty($shop_id)){
       $arrayVouchers = DB::table('vouchermaster')
         ->join('shops', 'shops.id', '=', 'vouchermaster.shop_id')
--- app/shops.php
+++ app/shops.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class shops extends Model
 {
-    //
+    public $table = 'shops';
+
+    public function shop(){
+	    return $this->hasMany('App\Account', 'shop_id', 'id');
+	}
 }
